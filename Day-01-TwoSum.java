7731ff89d295aaffc37f9fe7cb599faf4b586a22
Harshaghera111 <agheraharsh2@gmail.com>
2026-04-26 09:09:53 +0530

    Day 01 - Two Sum (HashMap)

---
+++ Day-01-TwoSum.java
@@ -0,0 +1,23 @@
+// Problem: Two Sum
+// Approach: HashMap
+// Time Complexity: O(n)
+
+import java.util.*;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            int need = target - nums[i];
+
+            if(map.containsKey(need)) {
+                return new int[]{map.get(need), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{-1, -1};
+    }
+}
